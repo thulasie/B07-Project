a4c4d9e6bcbd3f5753921e3774b5ad521ede8f7b
Mason Cho <masoncho@Masonui-MacBookAir.local>
2025-11-07 23:16:49 -0500

    Changed name from 'B07 Demo Summer 2024' to 'smartAir'

--- Starter Application/app/build.gradle.kts
+++ Starter Application/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.b07demosummer2024"
+    namespace = "com.example.smartAir"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.b07demosummer2024"
+        applicationId = "com.example.smartAir"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
