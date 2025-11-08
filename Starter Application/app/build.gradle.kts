362323c0ec752ff5c8e91ad0129d4bb0022e99c4
Mason Cho <masoncho@Masonui-MacBookAir.local>
2025-11-08 00:00:48 -0500

    Modified to connect firebase project

--- Starter Application/app/build.gradle.kts
+++ Starter Application/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.google.gms.google.services)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -30,7 +30,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
